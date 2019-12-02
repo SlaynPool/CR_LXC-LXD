e377374b108ac5ff7f790ecbdcfe976d1ec55509
SlaynPool <nicolas.vadkerti@gmail.com>
2019-12-02 10:25:39 +0100

    rm

--- init.sh
+++
@@ -1,6 +0,0 @@
-echo "# CR_LXC-LXD" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/SlaynPool/CR_LXC-LXD.git
-git push -u origin master
