c40c2bc545296090097638b44bcbfb20a964323e
SlaynPool <nicolas.vadkerti@gmail.com>
2019-12-02 10:24:16 +0100

    gitignore

---
+++ init.sh
@@ -0,0 +1,6 @@
+echo "# CR_LXC-LXD" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/SlaynPool/CR_LXC-LXD.git
+git push -u origin master
